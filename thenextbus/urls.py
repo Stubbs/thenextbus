2bcd0baa52ce7896ed488511191b0a2bc6ddae69
Stuart Grimshaw <stuart.github@smgsystems.co.uk>
2009-01-04 16:17:21 +0000

    Minor bug fixes, added email address.

--- thenextbus/urls.py
+++ thenextbus/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
 	(r'^$', 'thenextbus.stop.views.index'),
 	(r'^redirect/$', 'thenextbus.stop.views.redirect'),
+	(r'^stop/$', 'thenextbus.stop.views.redirect'),
 	(r'^stop/(?P<stop_number>\d+)/$', 'thenextbus.stop.views.stop'),
 )
