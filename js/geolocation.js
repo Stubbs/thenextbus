182450bafcf42eb233df5c6bcd52d323cc7db685
Stuart Grimshaw <stuart.github@smgsystems.co.uk>
2009-08-10 22:59:38 +0100

    JQuery method for getting data

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,15 +1,9 @@
 function handler(location) {
 	// Get the bus stops near here.
-	var url = "http://geocoding.cloudmade.com/b700b521f78b53b1b0e84cfabe192e6f/geocoding/geoobject_around_point/" + location.coords.latitude + "," + location.coords.longitude + "/500.js?object_type=bus_stop";
+	var url = "http://geocoding.cloudmade.com/b700b521f78b53b1b0e84cfabe192e6f/geocoding/geoobject_around_point/" + location.coords.latitude + ", " + location.coords.longitude + "/500.js?object_type=bus_stop";
 
-	var x = new Ajax.Request(url, {
-		method: 'get',
-		onSuccess: function(transport) {
-			alert(transport.status);
-		},
-		onFailure: function(transport) {
-			alert("JHHJGJ");
-		}});
+	$.get(url,function(data) {$.each(data.features, function(i, item){ 
+		alert(i); })});
 }
 
 function getLocation() {
@@ -17,4 +11,4 @@ function getLocation() {
 }
 
 // Add a listener to the "Near Me" button
-Event.observe(window, 'load', function() {Event.observe('nearme_button', 'click', getLocation)});
+$(document).ready(function() {$("#nearme_button").bind("click", getLocation)});
